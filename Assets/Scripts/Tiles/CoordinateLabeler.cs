f01eec678586a349fbf66e96751e5de98581d318
ArHiFranki <franki550@gmail.com>
2022-02-18 20:36:46 +0700

    Добавил изменение цвета текстовой метки тайла в зависимости от того, возможно на этом тайле разместить башню или нет. Добавил возможность включать/выключать текстовые метки тайла во время игры при помощи отладочной кнопки-переключателя.

--- Assets/Scripts/Tiles/CoordinateLabeler.cs
+++ Assets/Scripts/Tiles/CoordinateLabeler.cs
@@ -8,12 +8,19 @@ using System;
 [RequireComponent(typeof(TextMeshPro))]
 public class CoordinateLabeler : MonoBehaviour
 {
+    [SerializeField] private Color defaultColor = Color.white;
+    [SerializeField] private Color blockedColor = Color.grey;
+
     private TextMeshPro label;
     private Vector2Int coordinates = new Vector2Int();
+    private Waypoint waypoint;
 
     private void Awake()
     {
         label = GetComponent<TextMeshPro>();
+        label.enabled = false;
+
+        waypoint = GetComponentInParent<Waypoint>();
         DisplayCoordinates();
     }
 
@@ -24,6 +31,9 @@ public class CoordinateLabeler : MonoBehaviour
             DisplayCoordinates();
             UpdateObjectName();
         }
+
+        ColorCoordinates();
+        ToggleLabels();
     }
 
     private void DisplayCoordinates()
@@ -37,5 +47,25 @@ public class CoordinateLabeler : MonoBehaviour
     private void UpdateObjectName()
     {
         transform.parent.name = coordinates.ToString();
+    }
+
+    private void ColorCoordinates()
+    {
+        if (waypoint.IsPlaceable)
+        {
+            label.color = defaultColor;
+        }
+        else
+        {
+            label.color = blockedColor;
+        }
+    }
+
+    private void ToggleLabels()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            label.enabled = !label.IsActive();
+        }
     }
 }
--- Assets/Scripts/Tiles/Waypoint.cs
+++ Assets/Scripts/Tiles/Waypoint.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class Waypoint : MonoBehaviour
 {
     [SerializeField] private GameObject towerPrefab;
+
     [SerializeField] private bool isPlaceable;
+    public bool IsPlaceable { get { return isPlaceable; } }
 
     private void OnMouseDown()
     {
