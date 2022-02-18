4e3224288370cf26e9123cdf41846cb46761d7b1
ArHiFranki <franki550@gmail.com>
2022-02-18 20:08:20 +0700

    Добавил башням возможность стрельбы, добавил противнику систему здоровья и метод для его уменьшения

---
+++ Assets/Scripts/Enemy/EnemyHealth.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    [SerializeField] private int maxHitPoints = 5;
+
+    int currentHitPoints = 0;
+
+    private void Start()
+    {
+        currentHitPoints = maxHitPoints;
+    }
+
+    private void OnParticleCollision(GameObject other)
+    {
+        ProcessHit();
+    }
+
+    private void ProcessHit()
+    {
+        currentHitPoints--;
+
+        if (currentHitPoints <= 0)
+        {
+            gameObject.SetActive(false);
+            Destroy(gameObject);
+        }
+    }
+}
